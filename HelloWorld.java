216a4f199f88e2ebaa1e2d915cd68d9c1a758df5
shkwak <seunghwan@gmail.com>
2020-09-23 16:44:11 +0900

    kwak

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,5 @@ public class HelloWorld {
         System.out.println("Hello, world!");
         System.out.println("Hello, Branch!");
         System.out.println("Bye, Branch!");
-<<<<<<< HEAD
-        System.out.println("Seunghwan!");
-        System.out.println("kwak!");
-=======
-        System.out.println("KwakSeunghwan!");
->>>>>>> kwak
     }
 }
